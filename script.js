dabb0e8c7d953490decba2539c89daaf7ab7838e
Vladyslav-Markevych <vladen035@gmail.com>
2024-02-11 23:20:26 +0200

    First JS

---
+++ script.js
@@ -0,0 +1,39 @@
+// 1
+let a = "t" / 2;
+let b = "ami";
+console.log(a + b);
+
+// 2
+let Vladyslav = {
+  name: "Владислав",
+  nickname: "Vladen",
+  phone: 380931622120,
+  email: "Vladen035@gmail.com",
+  sex: "male",
+  hobby: "some text about my hobby",
+  "JS developer": true,
+};
+
+let Kate = {
+  name: "Катерина",
+  nickname: "Kate123",
+  phone: 380111111111,
+  email: "kate@gmail.com",
+  sex: "female",
+  hobby: "some text about hobby Kate",
+  "JS developer": false,
+};
+
+let Borys = {
+  name: "Борис",
+  nickname: "Borys123",
+  phone: 380222222222,
+  email: "Borys@gmail.com",
+  sex: "male",
+  hobby: "some text about hobby Borys",
+  "JS developer": false,
+};
+
+let array = [Vladyslav, Kate, Borys];
+
+console.log(array);
